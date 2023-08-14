22df1f35a099a76cc90d57a34704eb9a8e09dc68
Lukas Langen <lukas@langen.dev>
2023-08-14 17:10:28 +0200

    Add xfce as desktopManager

--- modules/gui.nix
+++ modules/gui.nix
@@ -5,14 +5,23 @@
     enable = true;
     autorun = true;
 
-    displayManager = {
-      sddm.enable = true;
-      defaultSession = "none+i3";
+    desktopManager = {
+        xterm.enable = false;
+        xfce = {
+            enable = true;
+            noDesktop = true;
+            enableXfwm = false;
+        };
     };
 
     windowManager.i3 = {
       enable = true;
       package = pkgs.i3-gaps;
+    };
+
+    displayManager = {
+      sddm.enable = true;
+      defaultSession = "xfce+i3";
     };
 
     libinput.enable = true;
